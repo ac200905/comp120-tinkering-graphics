06eb81c04b41fe63fa2d31ada75f1328840a68a4
ac200905 <ac200905@falmouth.ac.uk>
2017-10-24 16:40:35 +0100

    Changed comments in definitions from docstrings to # comments

--- Contract2-UnitReskin/Contract2-UnitReskin.py
+++ Contract2-UnitReskin/Contract2-UnitReskin.py
@@ -7,14 +7,14 @@ screen = pygame.display.set_mode((480, 100))
 
 """func to measure distance between two colors"""
 def color_distance(color_one, color_two):
-    """variables for getting RGB color values from each of the two colors"""
+    # variables for getting RGB color values from each of the two colors
     red1 = color_one.r
     blue1 = color_one.b
     green1 = color_one.g
     red2 = color_two.r
     blue2 = color_two.b
     green2 = color_two.g
-    """equation to find average of two colors"""
+    # equation to find average of two colors
     distance = math.sqrt((red1-red2)**2+(blue1-blue2)**2+(green1-green2)**2)
     return distance
 
@@ -44,14 +44,14 @@ color_yellow = pygame.Color(255, 255, 0)
 def unit_reskin_red(pic):
     for y in xrange(0, 100):
         for x in xrange(0, 480):
-            """extract the color from each pixel at each position (x,y)"""
+            # extract the color from each pixel at each position (x,y)
             original_color = pic.get_at((x, y))
-            """set variable for the color you want to contrast and compare"""
+            # set variable for the color you want to contrast and compare
             color_check = color_unskinned
-            """check the color of each pixel against unskinned color
-            (the color that you want to change)"""
+            # check the color of each pixel against unskinned color
+            # (the color that you want to change)
             if close_enough(original_color, color_check):
-                """if true, change those pixels to a new color"""
+                # if true, change those pixels to a new color
                 pic.set_at((x, y), color_red)
 
 
@@ -87,16 +87,16 @@ def unit_reskin_yellow(pic):
 
 """func to save each red unit in separate png files"""
 def save_red():
-    """set var for x axis start position for each unit on the
-    Units.png file"""
+    # set var for x axis start position for each unit on the
+    # Units.png file
     dist_crop = 0
-    """set for loop with range 0-4 to run for each of the 4 units in
-    Units.png file"""
+    # set for loop with range 0-4 to run for each of the 4 units in
+    # Units.png file
     for i in xrange(4):
-        """set var for separating units in Units.png file"""
+        # set var for separating units in Units.png file
         crop_rect = (dist_crop,0,120,100)
         dist_crop += 120
-        """create new subsurface to blit and save as new file"""
+        # create new subsurface to blit and save as new file
         cropped = pic.subsurface(crop_rect)
         screen.blit(cropped, (0, 0))
         pygame.image.save(cropped,  str(i + 1) + "_Red" + ".png")
