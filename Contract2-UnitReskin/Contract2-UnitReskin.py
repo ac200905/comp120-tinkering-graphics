2e89e1876a343ca2ae4d42365d13be0fb9a13920
ac200905 <ac200905@falmouth.ac.uk>
2017-10-24 16:28:26 +0100

    Changed spacing between function definitions and changed comments according to PEP-8

--- Contract2-UnitReskin/Contract2-UnitReskin.py
+++ Contract2-UnitReskin/Contract2-UnitReskin.py
@@ -1,53 +1,61 @@
 import math
 import pygame
-# initialize pygame and create screen
+"""initialize pygame and create screen"""
 pygame.init()
 screen = pygame.display.set_mode((480, 100))
 
-# func to measure distance between two colors
+
+"""func to measure distance between two colors"""
 def color_distance(color_one, color_two):
-    # variables for getting RGB color values from each of the two colors
+    """variables for getting RGB color values from each of the two colors"""
     red1 = color_one.r
     blue1 = color_one.b
     green1 = color_one.g
     red2 = color_two.r
     blue2 = color_two.b
     green2 = color_two.g
-    # equation to find average of two colors
+    """equation to find average of two colors"""
     distance = math.sqrt((red1-red2)**2+(blue1-blue2)**2+(green1-green2)**2)
     return distance
 
-# func returns True if two colors are sufficiently close (within a certain tolerance)
+
+"""func returns True if two colors are sufficiently close
+(within a certain tolerance)"""
 def close_enough(color_one, color_two):
     if color_distance(color_one, color_two) < 150.0:
         return True
     else:
         return False
 
-# variable 'pic' assigned to the original image file that contains the four unskinned units
+
+"""variable 'pic' assigned to the original image file that contains the
+four unskinned units"""
 pic = pygame.image.load('Units.png')
 
-# set variables for each color that will be used in later functions
+"""set variables for each color that will be used in later functions"""
 color_unskinned = pygame.Color(255,0,255)
 color_red = pygame.Color(255, 0, 0)
 color_green = pygame.Color(0, 255, 0)
 color_blue = pygame.Color(0, 0, 255)
 color_yellow = pygame.Color(255, 255, 0)
 
-# func to reskin units from original unskinned color (magenta) to red
+
+"""func to reskin units from original unskinned color (magenta) to red"""
 def unit_reskin_red(pic):
     for y in xrange(0, 100):
         for x in xrange(0, 480):
-            # extract the color from each pixel at each position (x,y)
+            """extract the color from each pixel at each position (x,y)"""
             original_color = pic.get_at((x, y))
-            # set variable for the color you want to contrast and compare
+            """set variable for the color you want to contrast and compare"""
             color_check = color_unskinned
-            # check the color of each pixel against unskinned color (the color that you want to change)
+            """check the color of each pixel against unskinned color
+            (the color that you want to change)"""
             if close_enough(original_color, color_check):
-                # if true, change those pixels to a new color
+                """if true, change those pixels to a new color"""
                 pic.set_at((x, y), color_red)
 
-# func to reskin units from red to green
+
+"""func to reskin units from red to green"""
 def unit_reskin_green(pic):
     for y in xrange(0, 100):
         for x in xrange(0, 480):
@@ -56,7 +64,8 @@ def unit_reskin_green(pic):
             if close_enough(original_color, color_check):
                 pic.set_at((x, y), color_green)
 
-# func to reskin units from green to blue
+
+"""func to reskin units from green to blue"""
 def unit_reskin_blue(pic):
     for y in xrange(0, 100):
         for x in xrange(0, 480):
@@ -65,7 +74,8 @@ def unit_reskin_blue(pic):
             if close_enough(original_color, color_check):
                 pic.set_at((x, y), color_blue)
 
-# func to reskin units from blue to yellow
+
+"""func to reskin units from blue to yellow"""
 def unit_reskin_yellow(pic):
     for y in xrange(0, 100):
         for x in xrange(0, 480):
@@ -74,21 +84,25 @@ def unit_reskin_yellow(pic):
             if close_enough(original_color, color_check):
                 pic.set_at((x, y), color_yellow)
 
-# func to save each red unit in separate png files
+
+"""func to save each red unit in separate png files"""
 def save_red():
-    # set var for x axis start position for each unit on the Units.png file
+    """set var for x axis start position for each unit on the
+    Units.png file"""
     dist_crop = 0
-    # set for loop with range 0-4 to run for each of the 4 units in Units.png file
+    """set for loop with range 0-4 to run for each of the 4 units in
+    Units.png file"""
     for i in xrange(4):
-        # set var for separating units in Units.png file
+        """set var for separating units in Units.png file"""
         crop_rect = (dist_crop,0,120,100)
         dist_crop += 120
-        # create new subsurface to blit and save as new file
+        """create new subsurface to blit and save as new file"""
         cropped = pic.subsurface(crop_rect)
         screen.blit(cropped, (0, 0))
         pygame.image.save(cropped,  str(i + 1) + "_Red" + ".png")
 
-# func to save each green unit in separate png files
+
+"""func to save each green unit in separate png files"""
 def save_green():
     dist_crop = 0
     for i in xrange(4):
@@ -98,7 +112,8 @@ def save_green():
         screen.blit(cropped, (0, 0))
         pygame.image.save(cropped, str(i + 1) + "_Green" + ".png")
 
-# func to save each blue unit in separate png files
+
+"""func to save each blue unit in separate png files"""
 def save_blue():
     dist_crop = 0
     for i in xrange(4):
@@ -108,7 +123,8 @@ def save_blue():
         screen.blit(cropped, (0, 0))
         pygame.image.save(cropped, str(i + 1) + "_Blue" + ".png")
 
-# func to save each yellow unit in separate png files
+
+"""func to save each yellow unit in separate png files"""
 def save_yellow():
     dist_crop = 0
     for i in xrange(4):
@@ -118,7 +134,8 @@ def save_yellow():
         screen.blit(cropped, (0, 0))
         pygame.image.save(cropped, str(i + 1) + "_Yellow" + ".png")
 
-# call functions in order to reskin and save files
+
+"""call functions in order to reskin and save files in order"""
 unit_reskin_red(pic)
 save_red()
 unit_reskin_green(pic)
