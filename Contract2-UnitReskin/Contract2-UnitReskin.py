0b265a9b75a807189a7ff2b9bc2c7a00fabd2791
proarua <nath_anc@hotmail.co.uk>
2017-10-25 14:47:48 +0100

    getting rid of duplicate code

--- Contract2-UnitReskin/Contract2-UnitReskin.py
+++ Contract2-UnitReskin/Contract2-UnitReskin.py
@@ -6,6 +6,8 @@ screen = pygame.display.set_mode((480, 100))
 
 
 """func to measure distance between two colors"""
+
+
 def color_distance(color_one, color_two):
     # variables for getting RGB color values from each of the two colors
     red1 = color_one.r
@@ -21,8 +23,10 @@ def color_distance(color_one, color_two):
 
 """func returns True if two colors are sufficiently close
 (within a certain tolerance)"""
+
+
 def close_enough(color_one, color_two):
-    if color_distance(color_one, color_two) < 150.0:
+    if color_distance(color_one, color_two) < 300.0:
         return True
     else:
         return False
@@ -39,54 +43,36 @@ color_green = pygame.Color(0, 255, 0)
 color_blue = pygame.Color(0, 0, 255)
 color_yellow = pygame.Color(255, 255, 0)
 
-
 """func to reskin units from original unskinned color (magenta) to red"""
-def unit_reskin_red(pic):
+
+
+def unit_reskin(pic,color):
     for y in xrange(0, 100):
         for x in xrange(0, 480):
             # extract the color from each pixel at each position (x,y)
             original_color = pic.get_at((x, y))
             # set variable for the color you want to contrast and compare
             color_check = color_unskinned
-            # check the color of each pixel against unskinned color
-            # (the color that you want to change)
+            # check the color of each pixel against unskinned color (the color that you want to change)
             if close_enough(original_color, color_check):
                 # if true, change those pixels to a new color
-                pic.set_at((x, y), color_red)
+                if color ==1:
+                    pic.set_at((x, y), color_red)
+                if color ==2:
+                    pic.set_at((x, y), color_green)
+                if color ==3:
+                    pic.set_at((x, y), color_blue)
+                if color ==4:
+                    pic.set_at((x, y), color_yellow)
+                else:
+                    print (color)
+                return color
 
 
-"""func to reskin units from red to green"""
-def unit_reskin_green(pic):
-    for y in xrange(0, 100):
-        for x in xrange(0, 480):
-            original_color = pic.get_at((x, y))
-            color_check = color_red
-            if close_enough(original_color, color_check):
-                pic.set_at((x, y), color_green)
+"""func to save each unit in separate png files"""
 
 
-"""func to reskin units from green to blue"""
-def unit_reskin_blue(pic):
-    for y in xrange(0, 100):
-        for x in xrange(0, 480):
-            original_color = pic.get_at((x, y))
-            color_check = color_green
-            if close_enough(original_color, color_check):
-                pic.set_at((x, y), color_blue)
-
-
-"""func to reskin units from blue to yellow"""
-def unit_reskin_yellow(pic):
-    for y in xrange(0, 100):
-        for x in xrange(0, 480):
-            original_color = pic.get_at((x, y))
-            color_check = color_blue
-            if close_enough(original_color, color_check):
-                pic.set_at((x, y), color_yellow)
-
-
-"""func to save each red unit in separate png files"""
-def save_red():
+def save(color):
     # set var for x axis start position for each unit on the
     # Units.png file
     dist_crop = 0
@@ -99,48 +85,30 @@ def save_red():
         # create new subsurface to blit and save as new file
         cropped = pic.subsurface(crop_rect)
         screen.blit(cropped, (0, 0))
-        pygame.image.save(cropped,  str(i + 1) + "_Red" + ".png")
-
-
-"""func to save each green unit in separate png files"""
-def save_green():
-    dist_crop = 0
-    for i in xrange(4):
-        crop_rect = (dist_crop,0,120,100)
-        dist_crop += 120
-        cropped = pic.subsurface(crop_rect)
-        screen.blit(cropped, (0, 0))
-        pygame.image.save(cropped, str(i + 1) + "_Green" + ".png")
-
-
-"""func to save each blue unit in separate png files"""
-def save_blue():
-    dist_crop = 0
-    for i in xrange(4):
-        crop_rect = (dist_crop,0,120,100)
-        dist_crop += 120
-        cropped = pic.subsurface(crop_rect)
-        screen.blit(cropped, (0, 0))
-        pygame.image.save(cropped, str(i + 1) + "_Blue" + ".png")
-
-
-"""func to save each yellow unit in separate png files"""
-def save_yellow():
-    dist_crop = 0
-    for i in xrange(4):
-        crop_rect = (dist_crop,0,120,100)
-        dist_crop += 120
-        cropped = pic.subsurface(crop_rect)
-        screen.blit(cropped, (0, 0))
-        pygame.image.save(cropped, str(i + 1) + "_Yellow" + ".png")
+        if color == 1:
+            pygame.image.save(cropped,  str(i + 1) + "_Red" + ".png")
+        if color == 2:
+            pygame.image.save(cropped, str(i + 1) + "_Green" + ".png")
+        if color == 3:
+            pygame.image.save(cropped, str(i + 1) + "_Blue" + ".png")
+        if color == 4:
+            pygame.image.save(cropped, str(i + 1) + "_Yellow" + ".png")
 
 
 """call functions in order to reskin and save files in order"""
-unit_reskin_red(pic)
-save_red()
-unit_reskin_green(pic)
-save_green()
-unit_reskin_blue(pic)
-save_blue()
-unit_reskin_yellow(pic)
-save_yellow()
+color = input("What color do you want the units? 1:Red 2:Green "
+              "3:Blue 4:Yellow")
+unit_reskin(pic,color)
+save(color);
+color = input("What color do you want the units? 1:Red 2:Green "
+              "3:Blue 4:Yellow")
+unit_reskin(pic,color)
+save(color);
+color = input("What color do you want the units? 1:Red 2:Green "
+              "3:Blue 4:Yellow")
+unit_reskin(pic,color)
+save(color);
+color = input("What color do you want the units? 1:Red 2:Green "
+              "3:Blue 4:Yellow")
+unit_reskin(pic,color)
+save(color);
